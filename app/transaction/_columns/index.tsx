4c4a898171784838d1ca659849c49c51f32412a0
Dion <dionn_a.p@hotmail.com>
2024-12-24 00:49:40 -0300

    feat: ensure transaction can be deleted

--- app/transaction/_columns/index.tsx
+++ app/transaction/_columns/index.tsx
@@ -3,13 +3,12 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Transaction } from "@prisma/client";
 import TransactionTypeBadge from "../_components/type-badge";
-import { Button } from "@/app/_components/ui/button";
-import { TrashIcon } from "lucide-react";
 import {
   TRANSACTION_CATEGORY_LABEL,
   TRANSACTION_PAYMENT_METHOD_LABEL,
 } from "@/app/_constants/transactions";
 import EditTransactionButton from "../_components/edit-transaction-button";
+import DeleteTransactionButton from "../_components/delete-transaction-button";
 
 export const transactionsColumns: ColumnDef<Transaction>[] = [
   {
@@ -65,9 +64,7 @@ export const transactionsColumns: ColumnDef<Transaction>[] = [
       return (
         <div>
           <EditTransactionButton transaction={transaction} />
-          <Button variant="ghost" size="icon" className="text-muted-foreground">
-            <TrashIcon />
-          </Button>
+          <DeleteTransactionButton transactionId={transaction.id} />
         </div>
       );
     },
